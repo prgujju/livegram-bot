a1a63997e34acb7d445112ce9b1d9737b0cd7bc6
prgujju <89723071+prgujju@users.noreply.github.com>
2023-04-27 12:06:54 +0530

    Update bot.ts

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -4,7 +4,7 @@ import process from 'process';
 if (!process.env.BOTTOKEN) throw new Error('BOT_TOKEN environment variable is required');
 if (!process.env.ADMIN) throw new Error('ADMIN environment variable is required');
 
-const bot: Bot<Context, Api<RawApi>> = new Bot(process.env.BOT_TOKEN);
+const bot: Bot<Context, Api<RawApi>> = new Bot(process.env.BOTTOKEN);
 
 bot.init()
   .then(() => console.log(`Bot has successfully launched`))
